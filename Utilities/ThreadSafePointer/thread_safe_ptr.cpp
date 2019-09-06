97cd1f2443a9b4b0ba1895dcb6a532cf91d29b3d
MohitBhola <mohit.bhola@gmail.com>
2019-09-06 16:13:13 +0530

    Update thread_safe_ptr.cpp
    Fixed indentation.

--- Utilities/ThreadSafePointer/thread_safe_ptr.cpp
+++ Utilities/ThreadSafePointer/thread_safe_ptr.cpp
@@ -4,154 +4,157 @@
 #include <map>
 #include <boost/operators.hpp>
 
+using namespace std;
+
 template <
-	typename T,
-	typename mutex_t = std::recursive_mutex,
-	typename lock_t = std::unique_lock<mutex_t>>
+    typename T,
+    typename mutex_t = std::recursive_mutex,
+    typename lock_t = std::unique_lock<mutex_t>>
 class thread_safe_ptr
 {
-	std::unique_ptr<T> ptr;
-	std::unique_ptr<mutex_t> mtx;
+    unique_ptr<T> ptr;
+    unique_ptr<mutex_t> mtx;
 
-	// the surrogate
-	template <
-		typename requested_lock_t>
-	class proxy 
-		: private boost::operators<proxy<requested_lock_t>>
-	{
-		T * const ptr{nullptr};
-		requested_lock_t lock {};
+    // the surrogate
+    template <
+        typename requested_lock_t>
+    class proxy 
+        : private boost::operators<proxy<requested_lock_t>>
+    {
+        T * const ptr{nullptr};
+        requested_lock_t lock {};
 
-	public:
+    public:
 
-		proxy(T * const p, mutex_t& mtx)
-		: ptr(p), lock(mtx) {}
+        proxy(T * const p, mutex_t& mtx)
+        : ptr(p), lock(mtx) {}
 
-		proxy(proxy&& rhs)
-		: ptr(std::move(rhs.ptr)), lock(std::move(rhs.lock)) {}
+        proxy(proxy&& rhs)
+        : ptr(move(rhs.ptr)), lock(move(rhs.lock)) {}
 
-		proxy& operator=(proxy const& rhs)
-		{
-			*ptr = *rhs.ptr;
-			return *this;
-		}
+        proxy& operator=(proxy const& rhs)
+        {
+            *ptr = *rhs.ptr;
+            return *this;
+        }
 
-		T* operator->() { return ptr; }
-		T const* operator->() const { return ptr; }
+        T* operator->() { return ptr; }
+        T const* operator->() const { return ptr; }
 
-		operator T&() { return *ptr; }
-		operator T const&() const { return *ptr; }
+        operator T&() { return *ptr; }
+        operator T const&() const { return *ptr; }
 
-		decltype(auto) operator[](std::size_t arg) {return (*ptr)[arg];}
+        decltype(auto) operator[](std::size_t arg) {return (*ptr)[arg];}
 
-		////////////////////////////////
-		// for boost::totally_ordered<T>
-    	////////////////////////////////
-		friend auto operator<(proxy const& lhs, proxy const& rhs)
-		{
-			return *lhs.ptr < *rhs.ptr;
-		}
+        ////////////////////////////////
+        // for boost::totally_ordered<T>
+        ////////////////////////////////
+        friend auto operator<(proxy const& lhs, proxy const& rhs)
+        {
+            return *lhs.ptr < *rhs.ptr;
+        }
 
-		friend auto operator==(proxy const& lhs, proxy const& rhs)
-		{
-			return *lhs.ptr == *rhs.ptr;
-		}
-		
-		///////////////////////////////////
-		// for boost::integer_arithmetic<T>
-    	///////////////////////////////////
-		proxy& operator+=(proxy const& rhs)
-		{
-			*ptr += *rhs.ptr;
-			return *this;
-		}
+        friend auto operator==(proxy const& lhs, proxy const& rhs)
+        {
+            return *lhs.ptr == *rhs.ptr;
+        }
+        
+        ///////////////////////////////////
+        // for boost::integer_arithmetic<T>
+        ///////////////////////////////////
+        proxy& operator+=(proxy const& rhs)
+        {
+            *ptr += *rhs.ptr;
+            return *this;
+        }
 
-		proxy& operator-=(proxy const& rhs)
-		{
-			*ptr -= *rhs.ptr;
-			return *this;
-		}
+        proxy& operator-=(proxy const& rhs)
+        {
+            *ptr -= *rhs.ptr;
+            return *this;
+        }
 
-		proxy& operator*=(proxy const& rhs)
-		{
-			*ptr *= *rhs.ptr;
-			return *this;
-		}
+        proxy& operator*=(proxy const& rhs)
+        {
+            *ptr *= *rhs.ptr;
+            return *this;
+        }
 
-		proxy& operator/=(proxy const& rhs)
-		{
-			*ptr /= *rhs.ptr;
-			return *this;
-		}
+        proxy& operator/=(proxy const& rhs)
+        {
+            *ptr /= *rhs.ptr;
+            return *this;
+        }
 
-		proxy& operator%=(proxy const& rhs)
-		{
-			*ptr %= *rhs.ptr;
-			return *this;
-		}
+        proxy& operator%=(proxy const& rhs)
+        {
+            *ptr %= *rhs.ptr;
+            return *this;
+        }
 
-		////////////////////////
-		// for boost::bitwise<T>
-    	////////////////////////
-		proxy& operator&=(proxy const& rhs)
-		{
-			*ptr &= *rhs.ptr;
-			return *this;
-		}
+        ////////////////////////
+        // for boost::bitwise<T>
+        ////////////////////////
+        proxy& operator&=(proxy const& rhs)
+        {
+            *ptr &= *rhs.ptr;
+            return *this;
+        }
 
-		proxy& operator|=(proxy const& rhs)
-		{
-			*ptr |= *rhs.ptr;
-			return *this;
-		}
+        proxy& operator|=(proxy const& rhs)
+        {
+            *ptr |= *rhs.ptr;
+            return *this;
+        }
 
-		///////////////////////////////
-		// for boost::unit_steppable<T>
-    	///////////////////////////////
-		proxy& operator++()
-		{
-			++(*ptr);
-			return *this;
-		}
+        ///////////////////////////////
+        // for boost::unit_steppable<T>
+        ///////////////////////////////
+        proxy& operator++()
+        {
+            ++(*ptr);
+            return *this;
+        }
 
-		proxy& operator--()
-		{
-			--(*ptr);
-			return *this;
-		}
-	};
+        proxy& operator--()
+        {
+            --(*ptr);
+            return *this;
+        }
+    };
 
 public:
 
-	template <
-		typename... Args>
-	thread_safe(Args... args)
-	: ptr(std::make_unique<T>(std::forward<Args>(args)...)), mtx(std::make_unique<mutex_t>()) {}
+    template <
+        typename... Args>
+    thread_safe(Args... args)
+    : ptr(make_unique<T>(forward<Args>(args)...)), mtx(make_unique<mutex_t>()) {}
 
-	void lock() {mtx->lock();}
-	void unlock() {mtx->unlock();}
-	auto try_lock() {return mtx->try_lock();}
+    void lock() {mtx->lock();}
+    void unlock() {mtx->unlock();}
+    auto try_lock() {return mtx->try_lock();}
 
-	auto operator->() {return proxy<lock_t>(ptr.get(), *mtx);}
-	auto const operator->() const {return proxy<lock_t>(ptr.get(), *mtx);}
+    auto operator->() {return proxy<lock_t>(ptr.get(), *mtx);}
+    auto const operator->() const {return proxy<lock_t>(ptr.get(), *mtx);}
 
-	auto operator*() {return proxy<lock_t>(ptr.get(), *mtx);}
-	auto const operator*() const {return proxy<lock_t>(ptr.get(), *mtx);}
+    auto operator*() {return proxy<lock_t>(ptr.get(), *mtx);}
+    auto const operator*() const {return proxy<lock_t>(ptr.get(), *mtx);}
 };
 
 class Foo
 {
-	int i{42}; 
-	float f{42.0}; 
-	char c{'a'}; 
+    int i{42}; 
+    float f{42.0}; 
+    char c{'a'}; 
 
 public:
 
-	 explicit Foo(int i_, float f_, char c_) : i(i_), f(f_), c(c_) {}
-	 void doSomething1() {}
-	 void doSomething2() {}
-	 void doSomething3() {}
+    explicit Foo(int i_, float f_, char c_) : i(i_), f(f_), c(c_) {}
+    void doSomething1() {}
+    void doSomething2() {}
+    void doSomething3() {}
 };
+
 
 // works with fundamental types 
 thread_safe_ptr<int> safeInt(42); 
@@ -168,45 +171,47 @@ thread_safe<string> safeStr2{"xyz"};
 
 void f1()
 {
-	// thread_safe_ptr<int> supports increment-assignment operator
-	// since the underlying int supports the same
-	*safeInt += 42; 
+    // thread_safe_ptr<int> supports increment-assignment operator
+    // since the underlying int supports the same
+    *safeInt += 42; 
 
-	// thread safe singular operation
-	safeFoo->doSomething1();
+    // thread safe singular operation
+    safeFoo->doSomething1();
 
-	// thread safe transactional semantics
-	// thread_safe_ptr implements the BasicLockable interface
-	{  
-		std::lock lk(safeFoo);
-		safeFoo->doSomething2();
-		safeFoo->doSomething3();
-	}
+    // thread safe transactional semantics
+    // thread_safe_ptr implements the BasicLockable interface
+    {  
+        std::lock lk(safeFoo);
+        safeFoo->doSomething2();
+        safeFoo->doSomething3();
+    }
 }
 
 void f2()
 {
-	// thread_safe_ptr<int> supports increment-assignment operator
-	// since the underlying int supports the same
-	*safeInt += 42;
+    // thread_safe_ptr<int> supports increment-assignment operator
+    // since the underlying int supports the same
+    *safeInt += 42;
 
-	// thread safe singular operation
-	safeFoo->doSomething2();
+    // thread safe singular operation
+    safeFoo->doSomething2();
 
-	// thread safe transactional semantics
-	// thread_safe_ptr implements the BasicLockable interface
-	{
-		std::lock lk(safeFoo);
-		safeFoo->doSomething2();
-		safeFoo->doSomething3();
-	}
+    // thread safe transactional semantics
+    // thread_safe_ptr implements the BasicLockable interface
+    {
+        std::lock lk(safeFoo);
+        safeFoo->doSomething2();
+        safeFoo->doSomething3();
+    }
 }
 
 // either f3 or f4 populates safeMap_copy
 // depending on which thread successfully acquires locks on both safeMap and safeMap_copy first
 
+
 // thread_safe_ptr<std::map> allows for assignment
 // since the underlying std::map supports the same
+
 
 // [SUBTLE]
 // we need to acquire a lock on both underlying shared resources (std::map)
@@ -218,69 +223,69 @@ void f2()
 
 void f3()
 {
-	{
-		std::lock(safeMap, safeMap_copy); // transactional semantics
-		if (*safeMap_copy.empty())
-		{
-			*safeMap_copy = *safeMap;
-		}
-	} 
+    {
+        std::lock(safeMap, safeMap_copy); // transactional semantics
+        if (*safeMap_copy.empty())
+        {
+            *safeMap_copy = *safeMap;
+        }
+    } 
 }
 
 void f4()
 {
-	{
-		std::lock(safeMap_copy, safeMap); // transactional semantics; note different order of lock acquisition than in f3()
-		if (*safeMap_copy.empty())
-		{
-			*safeMap_copy = *safeMap;
-		}
-	} 
+    {
+        std::lock(safeMap_copy, safeMap); // transactional semantics; note different order of lock acquisition than in f3()
+        if (*safeMap_copy.empty())
+        {
+            *safeMap_copy = *safeMap;
+        }
+    } 
 }
  
 int main() 
 { 
-	std::thread t1(f1);
-	std::thread t2(f2);
+    std::thread t1(f1);
+    std::thread t2(f2);
 
-	t1.join();
-	t2.join();
+    t1.join();
+    t2.join();
 
-	// thread_safe_ptr<int> allows access to the underlying shared resource (int) via dereferencing
-	// this is guaranteed to print 132 since increments happen
-	// atomically in threads t1 and t2
-	cout << *safeInt << '\n'; 
+    // thread_safe_ptr<int> allows access to the underlying shared resource (int) via dereferencing
+    // this is guaranteed to print 132 since increments happen
+    // atomically in threads t1 and t2
+    cout << *safeInt << '\n'; 
 
-	// thread_safe_ptr<Foo> allows for transparent indirection for member access
-	cout << safeFoo->c << '\n'; 
+    // thread_safe_ptr<Foo> allows for transparent indirection for member access
+    cout << safeFoo->c << '\n'; 
 
-	// thread_safe_ptr<std::map> allows for subscripting as the underlying 
-	// shared resource (a std::map) supports the same
-	(*safeMap)[1] = 1;
-	(*safeMap)[2] = 2; 
+    // thread_safe_ptr<std::map> allows for subscripting as the underlying 
+    // shared resource (a std::map) supports the same
+    (*safeMap)[1] = 1;
+    (*safeMap)[2] = 2; 
 
-	cout << (*safeMap)[1] << '\n';
-	cout << (*safeMap)[2] << '\n'; 
+    cout << (*safeMap)[1] << '\n';
+    cout << (*safeMap)[2] << '\n'; 
 
-	std::thread t3(f3);
-	std::thread t4(f4);
+    std::thread t3(f3);
+    std::thread t4(f4);
 
-	t3.join();
-	t4.join();
+    t3.join();
+    t4.join();
 
-	// safeMap_copy got populated in a thread safe manner 
-	// in either thread t3 or thread t4
-	cout << (*safeMap_copy)[1] << '\n'; 
-	cout << (*safeMap_copy)[2] << '\n'; 
+    // safeMap_copy got populated in a thread safe manner 
+    // in either thread t3 or thread t4
+    cout << (*safeMap_copy)[1] << '\n'; 
+    cout << (*safeMap_copy)[2] << '\n'; 
 
-	// thread_safe_ptr<std::string> allows for comparisons
-	// as the underlying shared resource (std::string) support the same
-	// std::lock used here for transactional semantics
-	{
-		std::lock lk(safeStr1, safeStr2); 
-		cout << boolalpha << (*safeStr1 > *safeStr2) << '\n';
-		cout << boolalpha << (*safeStr1 != *safeStr2) << '\n'; 
-	} 
+    // thread_safe_ptr<std::string> allows for comparisons
+    // as the underlying shared resource (std::string) support the same
+    // std::lock used here for transactional semantics
+    {
+        std::lock lk(safeStr1, safeStr2); 
+        cout << boolalpha << (*safeStr1 > *safeStr2) << '\n';
+        cout << boolalpha << (*safeStr1 != *safeStr2) << '\n'; 
+    } 
 
-	return 0; 
+    return 0; 
 } 
