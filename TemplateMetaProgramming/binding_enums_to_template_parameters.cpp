d10fd2c63de1923c35219bd74f40fe7e84c7aac2
Mohit Bhola <mohit.bhola@gmail.com>
2019-09-10 10:46:33 +0530

    Update binding_enums_to_template_parameters.cpp

--- TemplateMetaProgramming/binding_enums_to_template_parameters.cpp
+++ TemplateMetaProgramming/binding_enums_to_template_parameters.cpp
@@ -1,6 +1,6 @@
 /*
  * Binding an enum (named or unnamed) to a template parameter is futile
- * *if* we want to perform integer arithmetic on template function parameter(s)
+ * _if_ we intend to perform integer arithmetic on template function parameter(s)
  * or template function local variable(s)
  */
  
@@ -8,8 +8,6 @@ template<
     typename T>
 struct Foo
 {
-    // the compiler would never reserve storage for an enum
-    // it would be a compile time error for client code to take its address
     enum class Values {value = 42}; 
 };    
 
@@ -17,13 +15,14 @@ template<
     typename T>
 void f(T t)
 {
-    ++t; // ERROR
-    (void)(t+42); // ERROR
+    ++t;          // ERROR if T = named/unnamed enum
+    (void)(t+42); // ERROR if T = named/unnamed enum
 }    
 
 int main(int argc, char **argv)
 {
-    f(Foo<int>::Values::value);
+    f(Foo<int>::Values::value);                     // leads to ERRORs
+    //f(static_cast<int>(Foo<int>::Values::value)); // an explicit cast to an integral type is the right thing to do here
     
     return 0;
 }
