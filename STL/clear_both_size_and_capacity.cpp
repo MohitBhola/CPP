db4d2fa71f475d373fbbbbe5a5c8e7dad799e755
Mohit Bhola <mohit.bhola@gmail.com>
2019-09-16 14:12:46 +0530

    Create clear_both_size_and_capacity.cpp

---
+++ STL/clear_both_size_and_capacity.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    vector<int> ivec(42, 42);
+
+    cout << "Initial size: " << ivec.size() << '\n';
+    cout << "Initial capacity: " << ivec.capacity() << '\n';
+
+    // the standard idiom to clear off both size and capacity
+    // create a temporary vector whose both size and capacity are 0 (or maybe the capacity is some implementation defined minimum)
+    // then swap the *internals* of this vector with the target vector to *shrink*
+    vector<int>().swap(ivec);
+
+    cout << "Initial size: " << ivec.size() << '\n';
+    cout << "Initial capacity: " << ivec.capacity() << '\n';
+
+    return 0;
+}
