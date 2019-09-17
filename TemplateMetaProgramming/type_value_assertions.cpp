d27d2e6ffb260e57062234f6d5ce7aa978ec8d13
Mohit Bhola <mohit.bhola@gmail.com>
2019-09-17 11:12:46 +0530

    Update type_value_assertions.cpp

--- TemplateMetaProgramming/type_value_assertions.cpp
+++ TemplateMetaProgramming/type_value_assertions.cpp
@@ -1,3 +1,5 @@
+// sometimes, the simplest form of assertion just *tries to use* what we require
+
 template<
     std::size_t N>
 struct Foo
@@ -10,7 +12,10 @@ template<
     typename T>
 void Bar()
 {
+    // OK only *if* T has a publicly available member type type
     using ERROR_T_DOES_NOT_CONTAIN_type = typename T::type;
+    
+    // OK only *if* T has a publicly available member contant value
     decltype(T::value) ERROR_T_DOES_NOT_CONTAIN_value(T::value);
 }    
 
