99dbe487d7be78a59e7141d19f50646cacbe4462
Mohit Bhola <mohit.bhola@gmail.com>
2019-09-17 11:09:04 +0530

    Update no_type_yes_type.cpp

--- TemplateMetaProgramming/no_type_yes_type.cpp
+++ TemplateMetaProgramming/no_type_yes_type.cpp
@@ -1,3 +1,5 @@
+// a simple, portable method to have two *different* types
+// who could be distinguished on the basis of their size at compile time
 template<
     typename T>
 class larger_than
@@ -5,6 +7,7 @@ class larger_than
     T body_[2];
 };    
 
+// these are typically used to indicate success/failure of a compile time check in real programs
 using no_type = char;
 using yes_type = larger_than<no_type>;
 
