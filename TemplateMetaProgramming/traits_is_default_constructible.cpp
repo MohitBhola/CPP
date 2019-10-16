7652bee2d0c6750abe18fc8de7c1e4a612274be8
Mohit Bhola <mohit.bhola@gmail.com>
2019-10-16 06:19:15 +0530

    Update traits_is_default_constructible.cpp

--- TemplateMetaProgramming/traits_is_default_constructible.cpp
+++ TemplateMetaProgramming/traits_is_default_constructible.cpp
@@ -22,7 +22,6 @@ struct IsDefaultConstructible<T, std::void_t<decltype(T())>> : std::true_type
 
 int main()
 {
-    
     std::cout << IsDefaultConstructible<Foo>::value << '\n';
     std::cout << IsDefaultConstructible<Bar>::value << '\n';
     
