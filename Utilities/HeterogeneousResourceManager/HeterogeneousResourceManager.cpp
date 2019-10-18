75eb7025ed479b564a8aef0521e783c4fadc47be
Mohit Bhola <mohit.bhola@gmail.com>
2019-10-18 18:58:07 +0530

    Update HeterogeneousResourceManager.cpp

--- Utilities/HeterogeneousResourceManager/HeterogeneousResourceManager.cpp
+++ Utilities/HeterogeneousResourceManager/HeterogeneousResourceManager.cpp
@@ -8,47 +8,47 @@ class ResourceManager
 {
 private:
 
-	ResourceManager() = default;
+    ResourceManager() = default;
 
-	template <typename T>
-	static std::unordered_map<ResourceManager const*, std::vector<T>> resources{};
+    template <typename T>
+    static std::unordered_map<ResourceManager const*, std::vector<T>> resources{};
 
-	std::vector<std::function<void(ResourceManager const* rm)>> resourceReleaseFunctions{};
+    std::vector<std::function<void(ResourceManager const* rm)>> resourceReleaseFunctions{};
 
 public:
 
-	template <typename T>
-	void registerResource(T const& t)
-	{
-		if (resources<T>.find(this) == resources<T>.end())
-		{
-			resourceReleaseFunctions.push_back([](ResourceManager const* rm){resources<T>.erase(rm);});
-		}
+    template <typename T>
+    void registerResource(T const& t)
+    {
+        if (resources<T>.find(this) == resources<T>.end())
+        {
+            resourceReleaseFunctions.push_back([](ResourceManager const* rm){resources<T>.erase(rm);});
+        }
 
-		resources<T>[this].push_back(t);
-	}
+        resources<T>[this].push_back(t);
+    }
 
-	static ResourceManager& GetResourceManager()
-	{
-		static ResourceManager rm;
-		return rm;
-	}
+    static ResourceManager& GetResourceManager()
+    {
+        static ResourceManager rm;
+        return rm;
+    }
 
-	ResourceManager(ResourceManager const&) = delete;
-	ResourceManager(ResourceManager&&) = delete;
+    ResourceManager(ResourceManager const&) = delete;
+    ResourceManager(ResourceManager&&) = delete;
 
     void Clear()
     {
         for (auto const& resourceReleaseFunction : resourceReleaseFunctions)
-		{
-			resourceReleaseFunction(this);
-		}
+        {
+            resourceReleaseFunction(this);
+        }
     }
     
-	~ResourceManager()
-	{
-		Clear();
-	}
+    ~ResourceManager()
+    {
+        Clear();
+    }
 };
 
 template<class T>
