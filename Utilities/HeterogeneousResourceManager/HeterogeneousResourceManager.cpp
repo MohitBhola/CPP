0809b30ce7d06983c3972776015dd452ed0cbb4e
Mohit Bhola <mohit.bhola@gmail.com>
2019-10-20 07:53:35 +0530

    Update HeterogeneousResourceManager.cpp

--- Utilities/HeterogeneousResourceManager/HeterogeneousResourceManager.cpp
+++ Utilities/HeterogeneousResourceManager/HeterogeneousResourceManager.cpp
@@ -4,55 +4,104 @@
 #include <functional>
 #include <memory>
 
+template <typename...>
+using VoidT = void;
+
+template <typename T, typename = VoidT<>>
+struct HasReset : std::false_type
+{};
+
+template <typename T>
+struct HasReset<T, VoidT<decltype(std::declval<T>().reset())>> : std::true_type
+{};
+
+template <typename T, typename = VoidT<>>
+struct HasClear : std::false_type
+{};
+
+template <typename T>
+struct HasClear<T, VoidT<decltype(std::declval<T>().Clear())>> : std::true_type
+{};
+
 class ResourceManager
 {
 private:
 
-    ResourceManager() = default;
+	ResourceManager() = default;
 
-    template <typename T>
-    static std::unordered_map<ResourceManager const*, std::vector<T>> resources{};
+	template <typename T>
+	static std::unordered_map<ResourceManager const*, std::vector<T>> resourcesToBeReset{};
+	
+	template <typename T>
+	static std::unordered_map<ResourceManager const*, std::vector<T>> resourcesToBeCleared{};
 
-    std::vector<std::function<void(ResourceManager const* rm)>> resourceReleaseFunctions{};
+	std::vector<std::function<void(ResourceManager const* rm)>> resourceReleaseFunctions{};
 
 public:
 
-    template <typename T>
-    void registerResource(T const& t)
-    {
-        if (resources<T>.find(this) == resources<T>.end())
-        {
-            resourceReleaseFunctions.push_back([](ResourceManager const* rm){resources<T>.erase(rm);});
-        }
+	template <typename T>
+	std::enable_if_t<HasReset<T>::value> registerResource(std::reference_wrapper<T> t)
+	{
+		if (resourcesToBeReset<T>.find(this) == resourcesToBeReset<T>.end())
+		{
+			resourceReleaseFunctions.push_back([](ResourceManager const* rm)
+            {
+                for (auto& resource : resourcesToBeReset<T>[rm])
+                {
+                    resource.reset();
+                }
 
-        resources<T>[this].push_back(t);
-    }
+                resourcesToBeReset<T>.erase(rm);
+            });
+		}
 
-    static ResourceManager& GetResourceManager()
-    {
-        static ResourceManager rm;
-        return rm;
-    }
+		resourcesToBeReset<T>[this].push_back(t);
+	}
+	
+	template <typename T>
+	std::enable_if_t<HasClear<T>::value> registerResource(std::reference_wrapper<T> t)
+	{
+		if (resourcesToBeCleared<T>.find(this) == resourcesToBeCleared<T>.end())
+		{
+			resourceReleaseFunctions.push_back([](ResourceManager const* rm)
+            {
+                for (auto& resource : resourcesToBeCleared<T>[rm])
+                {
+                    resource.Clear();
+                }
 
-    ResourceManager(ResourceManager const&) = delete;
-    ResourceManager(ResourceManager&&) = delete;
+                resourcesToBeCleared<T>.erase(rm);
+            });
+		}
 
-    void Clear()
+		resourcesToBeCleared<T>[this].push_back(t);
+	}
+
+	static ResourceManager& GetResourceManager()
+	{
+		static ResourceManager rm;
+		return rm;
+	}
+
+	ResourceManager(ResourceManager const&) = delete;
+	ResourceManager(ResourceManager&&) = delete;
+
+    void Flush()
     {
         for (auto const& resourceReleaseFunction : resourceReleaseFunctions)
-        {
-            resourceReleaseFunction(this);
-        }
+		{
+			resourceReleaseFunction(this);
+		}
     }
     
-    ~ResourceManager()
-    {
-        Clear();
-    }
+	~ResourceManager() noexcept = default;
 };
 
 template<class T>
-std::unordered_map<ResourceManager const*, std::vector<T>> ResourceManager::resources;
+std::unordered_map<ResourceManager const*, std::vector<T>> ResourceManager::resourcesToBeReset;
+
+template<class T>
+std::unordered_map<ResourceManager const*, std::vector<T>> ResourceManager::resourcesToBeCleared;
 
 struct Foo
 {
@@ -70,13 +119,34 @@ struct Bar
     }
 };
 
+struct Baz
+{
+    ~Baz()
+    {
+        std::cout << "Baz getting destroyed" << '\n';
+    }
+    
+    void Clear()
+    {
+        std::cout << "Baz::Clear called" << '\n';
+    }
+};
 
 int main()
 {
-    ResourceManager::GetResourceManager().registerResource(std::make_shared<Foo>());
-    ResourceManager::GetResourceManager().registerResource(std::make_shared<Bar>());
+    auto pFoo = std::make_shared<Foo>();
+    auto pBar = std::make_shared<Bar>();
+    Baz aBaz{};
     
-    ResourceManager::GetResourceManager().Clear();
+    ResourceManager::GetResourceManager().registerResource(std::ref(pFoo));
+    ResourceManager::GetResourceManager().registerResource(std::ref(pBar));   
+    
+    ResourceManager::GetResourceManager().registerResource(std::ref(aBaz));   
+    
+    ResourceManager::GetResourceManager().Flush();
+    
+    std::cout << pFoo.use_count() << '\n';
+    std::cout << pBar.use_count() << '\n';
     
     return 0;
 }
