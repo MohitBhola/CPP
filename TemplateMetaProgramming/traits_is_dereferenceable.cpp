952d3a2690bcb7b281de9c1214cee6e7edba4634
Mohit Bhola <mohit.bhola@gmail.com>
2019-10-23 06:08:33 +0530

    Create traits_is_dereferenceable.cpp

---
+++ TemplateMetaProgramming/traits_is_dereferenceable.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <memory>
+#include <type_traits>
+#include <utility>
+using namespace std;
+
+template <typename T>
+class is_dereferenceable_helper
+{
+    template <typename U, typename = decltype(*declval<U>())>
+    static true_type test(U*);
+    
+    template <typename>
+    static false_type test(...);
+    
+public:
+
+    using type = decltype(test<T>(nullptr));
+};
+
+template <typename T>
+struct is_dereferenceable : is_dereferenceable_helper<T>::type
+{};
+
+int main()
+{
+    cout << is_dereferenceable<shared_ptr<int>>::value << '\n';
+    cout << is_dereferenceable<int>::value << '\n';
+    
+    return 0;
+}
