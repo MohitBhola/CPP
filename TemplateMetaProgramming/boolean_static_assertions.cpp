d949398391448bad4d5aba199b50408271d2ae48
Mohit Bhola <mohit.bhola@gmail.com>
2019-11-05 14:45:39 +0530

    Update boolean_static_assertions.cpp

--- TemplateMetaProgramming/boolean_static_assertions.cpp
+++ TemplateMetaProgramming/boolean_static_assertions.cpp
@@ -1,15 +1,16 @@
 // primary template
-template<
-    bool condition>
+// intentionally undefined
+template <bool condition>
 struct static_assertion;
 
-template<>
+// defined only for true conditions
+// so, attempt to instantiate the template static_assertion with a false condition won't compile
+template <>
 struct static_assertion<true>
 {
     static_assertion() = default;
     
-    template<
-        typename T>
+    template <typename T>
     static_assertion(T) {}
 };
 
@@ -22,3 +23,15 @@ int main()
       
     return 0;
 }
+
+/*
+main.cpp: In function 'int main()':
+main.cpp:21:50: error: variable 'const static_assertion<false> ASSERT2' has initializer but incomplete type
+   21 |     const static_assertion<(char(255)>0)> ASSERT2("char is unsigned");
+      |                                                  ^
+main.cpp:17:61: error: invalid application of 'sizeof' to incomplete type 'static_assertion<false>'
+   17 | #define ASSERT(condition) sizeof(static_assertion<condition>)
+      |                                                             ^
+main.cpp:22:5: note: in expansion of macro 'ASSERT'
+   22 |     ASSERT((char(255) > 0));
+*/
