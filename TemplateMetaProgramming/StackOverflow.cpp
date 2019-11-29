569e09f4a8aa21a025fccd372ef4d310276bdcb9
Mohit Bhola <mohit.bhola@gmail.com>
2019-11-29 15:43:11 +0530

    Update StackOverflow.cpp

--- TemplateMetaProgramming/StackOverflow.cpp
+++ TemplateMetaProgramming/StackOverflow.cpp
@@ -1,5 +1,7 @@
-// a universal copy constructor is never a copy constructor; the presence of the former doesn't suppresses the implicit declaration of the latter
-// universal constructors participate in overload resolution with other constructors, including copy constructors; they may be preferred over others if they provide a better match
+// a universal copy constructor is never a copy constructor
+// the presence of the former doesn't suppresses the implicit declaration of the latter
+// universal constructors participate in overload resolution with other constructors, including copy constructors
+// they may be preferred over others if they provide a better match
 
 struct base
 {
@@ -14,9 +16,13 @@ struct derived : base
 {
     derived() = default;
     
-    // an implicit copy constructor must invoke the copy constructor of the base class; it will never call a universal constructor
-    // however, if a user provided copy constructor for the derived class tries to invoke its base class counterpart, it can go horribly wrong in the presence of universal constructors
-    // here, the universal constructor of the base class gets called as it provides an exact match, but since it takes its argument by value, the call becomes recursive resulting in StackOverflow
+    // an implicit copy constructor must invoke the copy constructor of the base class
+    // it will never call a universal constructor
+    // however, if a user provided copy constructor for the derived class tries to invoke its base class counterpart, 
+    // it can go horribly wrong in the presence of universal constructors
+    
+    // here, the universal constructor of the base class gets called as it provides an exact match, 
+    // but since it takes its argument by value, the call becomes recursive resulting in StackOverflow!!!
     derived(derived const& that) : base(that)
     {}
 };
