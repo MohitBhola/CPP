691d93c3c4580d6fb42c39b401adb857297c9389
Mohit Bhola <mohit.bhola@gmail.com>
2019-12-02 12:26:12 +0530

    Update inserters.cpp

--- STL/inserters.cpp
+++ STL/inserters.cpp
@@ -12,6 +12,8 @@ int main()
     // back insertion
     istream_iterator<int> it_cin{cin};
     istream_iterator<int> it_cin_end{};
+    // back_inserter(...) is a convenience function template that constructs a std::back_insert_iterator 
+    // for the container c with the type deduced from the type of the argument
     copy(it_cin, it_cin_end, back_inserter(ideque));
     
     // middle insertion
@@ -20,10 +22,15 @@ int main()
     istringstream iss{"123 456 7890"};
     istream_iterator<int> it_iss{iss};
     istream_iterator<int> it_iss_end{};
+    
+    // inserter is a convenience function template that constructs a std::insert_iterator 
+    // for the container c and its iterator i with the type deduced from the type of the argument
     copy(it_iss, it_iss_end, inserter(ideque, next(begin(ideque), static_cast<difference_type>((ideque.size() / 2)))));
     
     // front insertion
     initializer_list<int> iil{-1, -2, -3};
+    // inserter is a convenience function template that constructs a std::insert_iterator 
+    // for the container c and its iterator i with the type deduced from the type of the argument
     copy(begin(iil), end(iil), front_inserter(ideque));
     
     // print
@@ -37,10 +44,7 @@ int main()
     return 0;
 }
 
-
-
 /*
 1 2 3 4 5
 -3	-2	-1	1	2	123	456	7890	3	4	5	
 */
-
