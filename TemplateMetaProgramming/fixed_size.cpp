e0320a220920a4e2c21b0831c93c1b4bf881429a
Mohit Bhola <mohit.bhola@gmail.com>
2020-01-02 18:34:54 +0530

    Update fixed_size.cpp

--- TemplateMetaProgramming/fixed_size.cpp
+++ TemplateMetaProgramming/fixed_size.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 // fixed_size<N> can itself have any size >= N
-// but, sizeof(fixed_size<N>::type == N), because sizeof(char) == 1, always, on all platforms
+// but, sizeof(fixed_size<N>::type) == N, because sizeof(char) == 1, always, on all platforms
 template <std::size_t N>
 struct fixed_size
 {
