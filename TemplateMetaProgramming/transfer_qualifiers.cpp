1c55cda7cd3ecd6d9124b5bd1d917c33201ef25e
Mohit Bhola <mohit.bhola@gmail.com>
2020-01-03 15:38:07 +0530

    Update transfer_qualifiers.cpp

--- TemplateMetaProgramming/transfer_qualifiers.cpp
+++ TemplateMetaProgramming/transfer_qualifiers.cpp
@@ -47,6 +47,14 @@ struct transfer_qualifiers<T1 const[N], T2>
     using type = aux_t[N];
 };
 
+// for bounded array of volatile
+template <typename T1, typename T2, int N>
+struct transfer_qualifiers<T1 volatile[N], T2>
+{
+    using aux_t = typename transfer_qualifiers<T1 volatile, T2>::type;
+    using type = aux_t[N];
+};
+
 // for unbounded array
 template <typename T1, typename T2>
 struct transfer_qualifiers<T1[], T2>
@@ -60,6 +68,14 @@ template <typename T1, typename T2>
 struct transfer_qualifiers<T1 const[], T2>
 {
     using aux_t = typename transfer_qualifiers<T1 const, T2>::type;
+    using type = aux_t[];
+};
+
+// for unbounded array of const
+template <typename T1, typename T2>
+struct transfer_qualifiers<T1 volatile[], T2>
+{
+    using aux_t = typename transfer_qualifiers<T1 volatile, T2>::type;
     using type = aux_t[];
 };
 
@@ -79,7 +95,6 @@ int main()
 
 /*
 main.cpp: In function 'int main()':
-main.cpp:75:43: error: aggregate 'TD<const double [4]> td' has incomplete type and cannot be defined
-   75 |     TD<transfer_qualifiers<T1, T2>::type> td;
+main.cpp:91:43: error: aggregate 'TD<const double [4]> td' has incomplete type and cannot be defined
+   91 |     TD<transfer_qualifiers<T1, T2>::type> td;
 */   
-
