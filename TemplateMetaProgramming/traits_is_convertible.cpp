03ff54f21275fc6b6f5398438fcebbbc284c01b5
Mohit Bhola <mohit.bhola@gmail.com>
2020-01-04 19:08:57 +0530

    Update traits_is_convertible.cpp

--- TemplateMetaProgramming/traits_is_convertible.cpp
+++ TemplateMetaProgramming/traits_is_convertible.cpp
@@ -2,18 +2,34 @@
 #include <utility>
 #include <type_traits>
 
+using namespace std;
+
 struct Base
 {};
 
 struct Derived : Base
 {};
 
-template <typename T>
-struct IsConvertibleHelperT
+template <typename FROM, typename TO>
+class IsConvertibleHelper
 {
-    static void aux(T);
+    template <typename X>
+    static true_type test(X);
+    
+    template <typename X>
+    static false_type test(...);
+    
+public: 
+
+    using type = decltype(test<TO>(declval<FROM>()));
 };
 
+template <typename FROM, typename TO>
+struct IsConvertible : IsConvertibleHelper<FROM, TO>::type
+{};
+
+// alternate solution based on SFINAE'ing out partial specialization
+/*
 template<
     typename FROM, 
     typename TO, 
@@ -25,14 +41,22 @@ struct IsConvertibleT : std::integral_constant<bool, std::is_void<FROM>::value &
 template <typename FROM, typename TO>
 struct IsConvertibleT<FROM, TO, false, std::void_t<decltype(IsConvertibleHelperT<TO>::aux(std::declval<FROM>()))>> : std::true_type
 {};
+*/
 
 int main()
 {
-    std::cout << IsConvertibleT<int, unsigned int>::value << '\n';
-    std::cout << IsConvertibleT<int, char*>::value << '\n';
+    std::cout << IsConvertible<int, unsigned int>::value << '\n';
+    std::cout << IsConvertible<int, char*>::value << '\n';
     
-    std::cout << IsConvertibleT<Derived, Base>::value << '\n';
-    std::cout << IsConvertibleT<Base, Derived>::value << '\n';
+    std::cout << IsConvertible<Derived, Base>::value << '\n';
+    std::cout << IsConvertible<Base, Derived>::value << '\n';
     
     return 0;
 }
+
+/*
+1
+0
+1
+0
+*/
