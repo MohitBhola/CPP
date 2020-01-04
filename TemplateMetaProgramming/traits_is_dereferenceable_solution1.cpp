ba46e8c002baab17ae001a835474d63a5936ebf5
Mohit Bhola <mohit.bhola@gmail.com>
2020-01-04 18:51:55 +0530

    Update traits_is_dereferenceable_solution1.cpp

--- TemplateMetaProgramming/traits_is_dereferenceable_solution1.cpp
+++ TemplateMetaProgramming/traits_is_dereferenceable_solution1.cpp
@@ -3,29 +3,18 @@
 #include <type_traits>
 #include <utility>
 
-template <std::size_t N>
-struct yes_type
-{};
-
-using no_type = char;
-
 template <typename T>
 class is_dereferenceable_helper
 {
-    template <typename U>
-    static yes_type<sizeof(*std::declval<U>())> test(U*);
+    template <typename U, typename = decltype(*std::declval<U>())>
+    static std::true_type test(U*);
     
     template <typename>
-    static no_type test(...);
-    
-    template <std::size_t N>
-    static std::true_type cast(yes_type<N>);
-    
-    static std::false_type cast(no_type);
+    static std::false_type test(...);
     
 public:
 
-    using type = decltype(cast(test<T>(nullptr)));
+    using type = decltype(test<T>(nullptr));
 };
 
 template <typename T>
