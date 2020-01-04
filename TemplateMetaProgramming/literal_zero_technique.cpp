1334a5e7340fc9b1705bbbdf0793a2c3888df49c
Mohit Bhola <mohit.bhola@gmail.com>
2020-01-04 19:26:05 +0530

    Update literal_zero_technique.cpp

--- TemplateMetaProgramming/literal_zero_technique.cpp
+++ TemplateMetaProgramming/literal_zero_technique.cpp
@@ -1,3 +1,8 @@
+// what if you want to be able to pass 0 but no other integer literal to a function?
+// literal zero technique to the rescue!
+// define a literal zero type as a pointer to an int member of a dummy class
+// such a type could only be instantiated with a literal 0 :)
+
 class dummy
 {};
 
@@ -8,8 +13,20 @@ void Foo(literal_zero_t zero)
 
 int main()
 {
-    Foo(0);
-    //Foo(42);
+    Foo(0);  // OK
+    Foo(42); // NOK
     
     return 0;
 }
+
+/*
+main.cpp: In function 'int main()':
+main.cpp:17:9: error: cannot convert 'int' to 'literal_zero_t' {aka 'int dummy::*'}
+   17 |     Foo(42); // NOK
+      |         ^~
+      |         |
+      |         int
+main.cpp:11:25: note:   initializing argument 1 of 'void Foo(literal_zero_t)'
+   11 | void Foo(literal_zero_t zero)
+      |          ~~~~~~~~~~~~~~~^~~~
+*/
