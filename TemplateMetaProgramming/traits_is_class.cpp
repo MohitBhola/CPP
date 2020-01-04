85d047c37779195770f39c1ef053a09b4a64aaa2
Mohit Bhola <mohit.bhola@gmail.com>
2020-01-04 18:38:08 +0530

    Update traits_is_class.cpp

--- TemplateMetaProgramming/traits_is_class.cpp
+++ TemplateMetaProgramming/traits_is_class.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+/*
 template <typename T, typename = void_t<>>
 struct IsClass : public false_type
 {};
@@ -8,22 +9,36 @@ struct IsClass : public false_type
 template <typename T>
 struct IsClass<T, void_t<int T::*>> : public true_type
 {};
+*/
 
-/*
 template <typename T>
-class IsClass
+struct YES
+{};
+
+using NO = char;
+
+template <typename T>
+class IsClassHelper
 {
     template <typename U>
-    static yes_type test(int U::*);
+    static YES<decltype(declval<int U::*>())> test(U*);
     
     template <typename U>
-    static no_type test(...);
+    static NO test(...);
+    
+    template <typename U>
+    static true_type cast(YES<U>);
+    
+    static false_type cast(NO);
     
 public:
 
-    static const int value = sizeof(test<T>(0)) != sizeof(no_type);
+    using type = decltype(cast(test<T>(nullptr)));
 };
-*/
+
+template <typename T>
+struct IsClass : IsClassHelper<T>::type
+{};
 
 struct Foo
 {};
@@ -34,3 +49,8 @@ int main()
     cout << boolalpha << IsClass<int>::value << '\n';
     return 0;
 }
+
+/*
+true
+false
+*/
