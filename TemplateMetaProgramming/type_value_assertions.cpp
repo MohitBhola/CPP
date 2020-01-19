c710774ddf76d63b9e81efb61741776ed539625a
Mohit Bhola <mohit.bhola@gmail.com>
2020-01-19 08:43:52 +0530

    Update type_value_assertions.cpp

--- TemplateMetaProgramming/type_value_assertions.cpp
+++ TemplateMetaProgramming/type_value_assertions.cpp
@@ -1,27 +1,31 @@
 // sometimes, the simplest form of assertion just *tries to use* what we require
+#include <cstdlib>
 
-template<
-    std::size_t N>
+template <std::size_t N>
 struct Foo
 {
     constexpr static std::size_t value = N;
     using type = char[N];
-};    
+}; 
 
-template<
-    typename T>
+template <typename T>
+void suppressUnusedWarning(T const& t)
+{}
+
+template <typename T>
 void Bar()
 {
-    // OK only *if* T has a publicly available member type type
+    // OK only if T has a publicly available member type type
     using ERROR_T_DOES_NOT_CONTAIN_type = typename T::type;
+    suppressUnusedWarning(ERROR_T_DOES_NOT_CONTAIN_type{});
     
-    // OK only *if* T has a publicly available member contant value
+    // OK only if T has a publicly available member constant value
     decltype(T::value) ERROR_T_DOES_NOT_CONTAIN_value(T::value);
+    suppressUnusedWarning(ERROR_T_DOES_NOT_CONTAIN_value);
 }    
 
 int main()
 {    
-    Bar<Foo<42>>(); // OK
-    
+    Bar<Foo<42>>(); // OK    
     return 0;
 }
