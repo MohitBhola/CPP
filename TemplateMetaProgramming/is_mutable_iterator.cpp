0ed676a40126f16d500d49ac86ca487005c27577
Mohit Bhola <mohit.bhola@gmail.com>
2020-02-06 11:03:10 +0530

    Update is_mutable_iterator.cpp

--- TemplateMetaProgramming/is_mutable_iterator.cpp
+++ TemplateMetaProgramming/is_mutable_iterator.cpp
@@ -3,6 +3,7 @@
 #include <iterator>
 #include <vector>
 #include <map>
+
 using namespace std;
 
 #define DEFINE_HAS_TYPE(MemType) \
@@ -33,33 +34,31 @@ template <typename T>
 struct is_iterator<T*> : true_type
 {};
 
-template <typename T1, typename T2>
-struct is_mutable_iterator_helper : false_type
-{};
-
-template <typename T>
-struct is_mutable_iterator_helper<T&, T> : true_type
-{};
-
-template <typename T, bool IS_ITERATOR = is_iterator<T>::value>
-class is_mutable_iterator
+template <typename iter_t>
+struct is_mutable_iterator_helper
 {
-    using value_t = typename iterator_traits<T>::value_type;
-    using reference_t = typename iterator_traits<T>::reference;
+    using val_t = typename iterator_traits<iter_t>::value_type;
     
-public:
-
-    static constexpr bool value = disjunction_v<
-                                    is_mutable_iterator_helper<reference_t, value_t>,
-                                    bool_constant<is_convertible_v<reference_t, value_t> && !is_convertible_v<value_t, reference_t>>>;
+    // expression SFINAE with decltype
+    // an iterator is mutable if we can assign to the result of dereferencing it
+    template <typename I, typename V>
+    static auto test(void*) -> decltype(*std::declval<I>() = std::declval<V>(), true_type());
+    
+    template<typename I, typename V>
+    static false_type test(...);
+    
+    using type = decltype(test<iter_t, val_t>(nullptr));
 };
 
+// if iter_t is indeed an iterator, proceed for the real test
+template <typename iter_t, bool IS_ITERATOR = is_iterator<iter_t>::value>
+struct is_mutable_iterator : is_mutable_iterator_helper<iter_t>::type
+{};
+
+// if iter_t isn't even an iterator
 template <typename T>
-class is_mutable_iterator<T, false>
-{
-public:
-    static constexpr bool value = false;
-};
+struct is_mutable_iterator<T, false> : std::false_type
+{};
 
 struct Foo
 {};
@@ -67,15 +66,22 @@ struct Foo
 int main()
 {
     cout << boolalpha << is_mutable_iterator<int>::value << '\n';
+    cout << "=====" << '\n';
+    
+    cout << boolalpha << is_mutable_iterator<Foo>::value << '\n';
+    cout << "=====" << '\n';
     
     using ivec_iterator_t = vector<int>::iterator;
     cout << boolalpha << is_mutable_iterator<ivec_iterator_t>::value << '\n';
+    cout << "=====" << '\n';
     
     using ivec_const_iterator_t = vector<int>::const_iterator;
     cout << boolalpha << is_mutable_iterator<ivec_const_iterator_t>::value << '\n';
+    cout << "=====" << '\n';
     
     using imap_iterator_t = map<int, int>::iterator;
     cout << boolalpha << is_mutable_iterator<imap_iterator_t>::value << '\n';
+    cout << "=====" << '\n';
     
     using imap_const_iterator_t = map<int, int>::const_iterator;
     cout << boolalpha << is_mutable_iterator<imap_const_iterator_t>::value << '\n';
@@ -85,8 +91,14 @@ int main()
 
 /*
 false
-true
+=====
 false
+=====
 true
+=====
+false
+=====
+false
+=====
 false
 */
